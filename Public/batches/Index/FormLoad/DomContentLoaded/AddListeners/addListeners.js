3ae6e316e42cf2beb08fbf5d91ccf7ee8d9151b3
keshavsoft <nknnkumar@yahoo.com>
2026-08-08 16:52:17 +0530

    search

--- Public/batches/Index/FormLoad/DomContentLoaded/AddListeners/addListeners.js
+++ Public/batches/Index/FormLoad/DomContentLoaded/AddListeners/addListeners.js
@@ -1,12 +1,15 @@
 import showAllHtmlId from "./showAllHtmlId/start.js";
 import crudHtmlId from "./crudHtmlId/start.js";
 import verticalHtmlId from './verticalHtmlId/start.js';
+import searchHtmlId from './searchHtmlId/start.js';
 
 const hookAllListeners = () => {
 
     verticalHtmlId();
     showAllHtmlId();
     crudHtmlId();
+    searchHtmlId();
+
 };
 
 export default hookAllListeners;
