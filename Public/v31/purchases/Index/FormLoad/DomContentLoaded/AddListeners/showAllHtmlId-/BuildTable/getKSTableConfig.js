7f7cd7b9ab2f018376175d4f570d2d9b3a5b151b
keshavsoft <nknnkumar@yahoo.com>
2026-08-11 14:27:34 +0530

    public del

--- Public/v31/purchases/Index/FormLoad/DomContentLoaded/AddListeners/showAllHtmlId-/BuildTable/getKSTableConfig.js
+++
@@ -1,9 +0,0 @@
-import configJson from "/Index/configs/billShow.json" with { type: "json" };
-
-// import configJson from "../../../../../configs/billShow.json" with { type: "json" };
-
-export const getKSTableConfig = async () => {
-    let fromConfig = structuredClone(configJson);
-
-    return fromConfig
-};
