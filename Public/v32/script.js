7846ec3c81ec29313d66bc2e20d0ae4e354cbaa6
keshavsoft <nknnkumar@yahoo.com>
2026-08-12 14:38:28 +0530

    comp

--- Public/v32/script.js
+++ Public/v32/script.js
@@ -88,7 +88,7 @@ const ensureKSComponents = () => loadResource({
     name: "KSComponents",
     sources: [
         { type: "js", url: "/ks/components/v23/index.js", label: "Local" },
-        { type: "js", url: "https://keshavsoft.github.io/ks-web-comp-table/dist/v3.22/KSComponents.js", label: "git" }
+        { type: "js", url: "https://keshavsoft.github.io/ks-web-comp-table/dist/v3.23/KSComponents.js", label: "git" }
     ]
 });
 
