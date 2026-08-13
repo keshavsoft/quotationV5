f3a95ea4613c949bfc5309bdfcfc341c667d45ec
keshavsoft <nknnkumar@yahoo.com>
2026-08-13 12:39:08 +0530

    v3

--- api/routes.js
+++ api/routes.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import { router as routerFromv1 } from './v1/routes.js';
 import { router as routerFromv2 } from './v2/routes.js';
+import { router as routerFromv3 } from './v3/routes.js';
 
 const router = express.Router();
 
 router.use("/v1", routerFromv1);
 router.use("/v2", routerFromv2);
+router.use("/v3", routerFromv3);
 
 export { router };
