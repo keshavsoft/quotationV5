ee6b2a56aaa9f0cf139ed77f3e347e55754b27f8
keshavsoft <nknnkumar@yahoo.com>
2026-08-19 10:36:20 +0530

    docs

--- Public/ks/jsTableBuilder/v10/buildTable/forHead/appendHeaderCells.js
+++ Public/ks/jsTableBuilder/v10/buildTable/forHead/appendHeaderCells.js
@@ -1,14 +1,17 @@
 import buildHeaderCell from "./HeaderCell.js";
+const showLog = true;
 
-export function appendHeaderCells({ 
-    inHeaderRowElement, 
-    inColumns, 
-    inClasses = {}, 
-    inSortState = [], 
-    inOnSort = () => {} 
+export function appendHeaderCells({
+    inHeaderRowElement,
+    inColumns,
+    inClasses = {},
+    inSortState = [],
+    inOnSort = () => { }
 }) {
     inColumns.forEach(columnData => {
-        const headerCellElement = buildHeaderCell({ 
+        if (showLog) console.log("appendHeaderCells -> columnData", columnData);
+
+        const headerCellElement = buildHeaderCell({
             inHeader: columnData.header,
             inDataKey: columnData.dataKey,
             inOptions: columnData.options || {},
